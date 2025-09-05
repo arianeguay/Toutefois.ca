de36423bac6ddfe6cf53afef274077a7d73dc363
Ariane Guay <ariane.dguay@gmail.com>
2025-09-05 14:24:15 -0400

    Deploy Nginx

--- frontend/src/layout/Header/styles.tsx
+++ frontend/src/layout/Header/styles.tsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+width: 100%;
+background-color: ${props => props.theme.colors.headerBackground};
+
+`;
---
+++ frontend/src/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+        primaryText: "#2F2C58",
+        secondaryText: "#6B1E2C",
+        lightText: "#F5F3EE",
+
+        headerBackground: "#2F2C58",
+    },
+};
+
+export default theme;
