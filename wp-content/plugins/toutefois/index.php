d625df901466fdffd1eb0ac2e5388d3334238103
Ariane Guay <ariane.dguay@gmail.com>
2025-09-06 18:41:51 -0400

    Refactor: Remove PHP CORS handling to rely on Nginx

--- wp-content/plugins/toutefois/index.php
+++ wp-content/plugins/toutefois/index.php
@@ -295,29 +295,6 @@ add_action('rest_api_init', function () {
 });
 
 
-// 10. Add CORS headers specifically for REST API requests
-function add_cors_rest_headers($response, $handler, $request) {
-    $allowed_origins = array(
-        'https://toutefois.arianeguay.ca',
-        'https://admin.toutefois.arianeguay.ca',
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://localhost'
-    );
-    
-    $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-    
-    if (in_array($origin, $allowed_origins)) {
-        $response->header('Access-Control-Allow-Origin', $origin);
-    }
-    
-    $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-WP-Nonce');
-    $response->header('Access-Control-Allow-Credentials', 'true');
-    
-    return $response;
-}
-add_filter('rest_pre_serve_request', 'add_cors_rest_headers', 10, 3);
 
 // 11. Register Gutenberg Blocks
 function toutefois_register_blocks() {
