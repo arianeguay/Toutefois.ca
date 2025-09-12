937d2c585b4814ee63fe5af991d3d70c508d5f1c
Ariane Guay <ariane.dguay@gmail.com>
2025-09-12 12:19:29 -0400

    Removed N

--- wp-content/plugins/toutefois/src/collaborators/render.php
+++ wp-content/plugins/toutefois/src/collaborators/render.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Collaborators Block Template.
  */
@@ -67,7 +68,7 @@ $data_attributes = [
 $wrapper_attributes = get_block_wrapper_attributes();
 
 echo sprintf(
-    '<div %1$s>\n    <div class="toutefois-collaborators-block-react-root" data-props=\'%2$s\'></div>\n</div>',
+    '<div %1$s><div class="toutefois-collaborators-block-react-root" data-props=\'%2$s\'></div></div>',
     $wrapper_attributes,
     esc_attr(wp_json_encode($data_attributes))
 );
