0308e5ad0d66f7cf97f5e728d438d8537df53294
Ariane Guay <ariane.dguay@gmail.com>
2025-09-29 16:05:05 -0400

    Build plugin on ci

--- wp-content/plugins/toutefois/build/featured-carousel/save.js
+++
@@ -1,8 +0,0 @@
-import { useBlockProps } from "@wordpress/block-editor";
-
-export default function Save() {
-  const blockProps = useBlockProps.save({
-    'data-dynamic-block': 'true'
-  });
-  return <div {...blockProps}></div>;
-}
