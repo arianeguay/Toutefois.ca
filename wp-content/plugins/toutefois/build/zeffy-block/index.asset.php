558b2dcf31a0912282d56056de655aca4ffff367
Ariane Guay <ariane.dguay@gmail.com>
2025-09-29 13:37:33 -0400

    build zeffy block

--- wp-content/plugins/toutefois/build/zeffy-block/index.asset.php
+++ wp-content/plugins/toutefois/build/zeffy-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8f232618a90789662b8f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '853d5c45ea879eb9f78f');
