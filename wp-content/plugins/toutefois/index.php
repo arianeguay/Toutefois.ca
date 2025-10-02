82c18921929cf7d736cdb3fcc28b9443c6c54edc
Ariane Guay <ariane.dguay@gmail.com>
2025-10-02 17:23:01 -0400

    fix

--- wp-content/plugins/toutefois/index.php
+++ wp-content/plugins/toutefois/index.php
@@ -39,11 +39,12 @@ require_once plugin_dir_path(__FILE__) . 'inc/main-projects.php';
 // Facebook sync (imports Facebook posts as WordPress news with cron and REST trigger)
 require_once plugin_dir_path(__FILE__) . 'facebook-sync.php';
 
+
 // Add custom page templates
 function toutefois_add_page_templates($templates, $theme, $post)
 {
     if ($post && in_array($post->post_type, ['page', 'projet'])) {
-
+        $templates['template-no-margin.php'] = __('No Margin', 'toutefois');
         $templates['template-banner.php'] = __('With Banner', 'toutefois');
         $templates['template-title.php'] = __('With Title', 'toutefois');
     }
