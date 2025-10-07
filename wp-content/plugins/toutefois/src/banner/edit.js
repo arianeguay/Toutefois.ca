30c56f5a70577689925c172aad4ac8b80c57890d
Ariane Guay <ariane.dguay@gmail.com>
2025-10-07 10:26:29 -0400

    fixed banner render

--- wp-content/plugins/toutefois/src/banner/edit.js
+++ wp-content/plugins/toutefois/src/banner/edit.js
@@ -24,6 +24,7 @@ export default function Edit({ attributes, setAttributes }) {
     horizontalAlignment,
     objectPosition,
     textColor,
+    bigTextShadow,
     textShadowColor,
     template,
     blurredBackground,
@@ -37,13 +38,20 @@ export default function Edit({ attributes, setAttributes }) {
   // Wrapper props with data-attributes for theme CSS hooks
   const wrapperProps = useBlockProps({
     className: "wp-block-toutefois-banner",
-{{ ... }}
+    style: {
+      position: "relative",
+      height: height || "350px",
+      width: "100%",
+      marginBottom: "24px",
+    },
     "data-va": verticalAlignment,
     "data-ha": horizontalAlignment,
     "data-blurred": blurredBackground ? "1" : "0",
     "data-height": height,
     "data-height-unit": heightUnit,
     "data-background-mode": backgroundMode,
+    "data-big-text-shadow": bigTextShadow ? "1" : "0",
+    "data-background-svg": backgroundSvg ? "1" : "0",
   });
 
   // Background preview styles based on mode
@@ -82,7 +90,53 @@ export default function Edit({ attributes, setAttributes }) {
     position: "absolute",
     top: 0,
     left: 0,
-{{ ... }}
+    right: 0,
+    bottom: 0,
+    zIndex: 1,
+    padding: "16px",
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    alignItems: horizontalAlignment,
+    justifyContent: verticalAlignment,
+    textAlign:
+      horizontalAlignment === "flex-start"
+        ? "left"
+        : horizontalAlignment === "flex-end"
+        ? "right"
+        : "center",
+    color: textColor,
+  };
+
+  const headingStyle = {
+    width: "fit-content",
+    fontFamily: font || "Poppins, sans-serif",
+    textShadow: bigTextShadow
+      ? `4px 3px 0px ${textShadowColor}`
+      : `1px 1px 3px ${textShadowColor}`,
+    margin: 0,
+  };
+
+  return (
+    <>
+      <InspectorControls>
+        <PanelBody title={__("Banner Settings", "toutefois")}>
+          <TextControl
+            label={__("Title", "toutefois")}
+            value={title}
+            onChange={(value) => setAttributes({ title: value })}
+          />
+          <TextControl
+            label={__("Description", "toutefois")}
+            value={description}
+            onChange={(value) => setAttributes({ description: value })}
+          />
+          <SelectControl
+            label={__("Template", "toutefois")}
+            value={template}
+            onChange={(value) => setAttributes({ template: value })}
+            options={[
+              { label: "Default", value: "default" },
               { label: "No Margin", value: "no-margin" },
               { label: "With Banner", value: "with-banner" },
               { label: "With Title", value: "with-title" },
@@ -111,60 +165,77 @@ export default function Edit({ attributes, setAttributes }) {
             value={font}
             onChange={(value) => setAttributes({ font: value })}
             options={[
-{{ ... }}
+              { label: "Poppins", value: "Poppins" },
+              { label: "Cerebri", value: "Cerebri" },
+              { label: "Gagalin", value: "Gagalin" },
+              { label: "Montserrat", value: "Montserrat" },
+            ]}
+          />
+          <SelectControl
+            label={__("Vertical Alignment", "toutefois")}
+            value={verticalAlignment}
+            onChange={(value) => setAttributes({ verticalAlignment: value })}
+            options={[
+              { label: "Top", value: "flex-start" },
+              { label: "Center", value: "center" },
+              { label: "Bottom", value: "flex-end" },
+            ]}
+          />
+          <SelectControl
+            label={__("Horizontal Alignment", "toutefois")}
+            value={horizontalAlignment}
+            onChange={(value) => setAttributes({ horizontalAlignment: value })}
+            options={[
               { label: "Left", value: "flex-start" },
               { label: "Center", value: "center" },
               { label: "Right", value: "flex-end" },
             ]}
           />
-          {backgroundMode === "image" && (
-            <>
-              <MediaUploadCheck>
-                <MediaUpload
-                  onAllowedTypes={["image"]}
-                  value={image ? image.id : ""}
-                  onSelect={onSelectImage}
-                  render={({ open }) => (
-                    <Button onClick={open} isPrimary>
-                      {__("Choose Image", "toutefois")}
-                    </Button>
-                  )}
-                />
-              </MediaUploadCheck>
-              <TextControl
-                label={__("Image Position", "toutefois")}
-                value={objectPosition}
-                onChange={(value) => setAttributes({ objectPosition: value })}
-                help={__('e.g., "center top", "25% 75%"', "toutefois")}
-              />
-            </>
-          )}
-          {(backgroundMode === "color" || backgroundMode === "image" || backgroundMode === "svg") && (
-            <>
-              <p>
-                <strong>{__("Background Color", "toutefois")}</strong>
-              </p>
-              <ColorPalette
-                value={backgroundColor}
-                onChange={(value) => setAttributes({ backgroundColor: value })}
-              />
-            </>
-          )}
-          {backgroundMode === "svg" && (
-            <TextControl
-              label={__("SVG Markup", "toutefois")}
-              help={__("Paste inline SVG markup.", "toutefois")}
-              value={backgroundSvg}
-              onChange={(value) => setAttributes({ backgroundSvg: value })}
-              rows={4}
-            />
-          )}
           <p>
             <strong>{__("Text Color", "toutefois")}</strong>
           </p>
           <ColorPalette
             value={textColor}
-{{ ... }}
+            onChange={(value) => setAttributes({ textColor: value })}
+          />
+          <ToggleControl
+            label={__("Big Text Shadow", "toutefois")}
+            checked={bigTextShadow}
+            onChange={() => setAttributes({ bigTextShadow: !bigTextShadow })}
+          />
+          <p>
+            <strong>{__("Text Shadow Color", "toutefois")}</strong>
+          </p>
+          <ColorPalette
+            value={textShadowColor}
+            onChange={(value) => setAttributes({ textShadowColor: value })}
+          />
+          <div style={{ marginTop: "16px" }}>
+            <TextControl
+              label={__('Banner Height', 'toutefois')}
+              type="number"
+              value={height?.replace ? height.replace(/[^0-9.]/g, '') : height}
+              onChange={(value) => {
+                const numValue = parseFloat(value);
+                if (!isNaN(numValue) && numValue > 0) {
+                  setAttributes({ height: `${numValue}${heightUnit}` });
+                }
+              }}
+            />
+            <SelectControl
+              label={__('Height Unit', 'toutefois')}
+              value={heightUnit}
+              options={[
+                { label: 'px', value: 'px' },
+                { label: '%', value: '%' },
+                { label: 'vh', value: 'vh' },
+                { label: 'rem', value: 'rem' },
+              ]}
+              onChange={(value) => {
+                setAttributes({ heightUnit: value });
+                const heightValue = height?.replace ? parseFloat(height.replace(/[^0-9.]/g, '')) : 350;
+                setAttributes({ height: `${heightValue}${value}` });
+              }}
             />
           </div>
         </PanelBody>
@@ -184,7 +255,20 @@ export default function Edit({ attributes, setAttributes }) {
             className="toutefois-banner__body"
             style={{
               display: "flex",
-{{ ... }}
+              flexDirection: "column",
+              height: "100%",
+              alignItems: horizontalAlignment,
+              justifyContent: verticalAlignment,
+              textAlign:
+                horizontalAlignment === "flex-start"
+                  ? "left"
+                  : horizontalAlignment === "flex-end"
+                  ? "right"
+                  : "center",
+              color: textColor,
+            }}
+          >
+            <h1 className="toutefois-banner__title" style={headingStyle}>
               {title}
             </h1>
             <p className="toutefois-banner__description" style={{ width: "fit-content" }}>
@@ -200,4 +284,3 @@ export default function Edit({ attributes, setAttributes }) {
       </div>
     </>
   );
-}
